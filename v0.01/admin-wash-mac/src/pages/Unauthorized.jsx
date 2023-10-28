c0819f803990ff069761e9cd950cf3e3ddeaf7a6
badebess <88638253+badebess@users.noreply.github.com>
2023-10-28 21:16:24 +0700

    fix (Unauthorized page) : redesign page

--- v0.01/admin-wash-mac/src/pages/Unauthorized.jsx
+++ v0.01/admin-wash-mac/src/pages/Unauthorized.jsx
@@ -1,13 +1,14 @@
-import { ContainerOutlined } from "@ant-design/icons";
+import unauth  from "../assets/unauth.png";
 import React from "react";
 import { Link } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.css';
 
 export function Unauthorized() {
 	return (
-		<div>
-			<h1>Unauthorized</h1>
-			<p>401</p>
-			<phai></phai>
+		<div className="position-absolute top-50 start-50 translate-middle text-center">
+			<h3>Oopss, you're not in login</h3>
+			<img src={unauth} style={{width:"1000px"}}></img>
+			<h3>Please login first 😊</h3>
 			<Link to="/login">Login</Link>
 		</div>
 	);
